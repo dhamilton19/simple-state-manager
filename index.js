0c62420f7eaca5496f401f322b6690b2069e72b5
dhamilton91 <dhamilton91@hotmail.com>
2016-04-09 11:39:23 +0100

    updates

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+exports.AppState = undefined;
+
+var _AppState = require('./AppState');
+
+var _AppState2 = _interopRequireDefault(_AppState);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.AppState = _AppState2.default;
